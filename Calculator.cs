13ec4c498339cfa196ab154c22c0c8ba8469fd4c
al-baihaqi-192 <m.abdai@students.amikom.ac.id>
2026-04-08 15:55:40 +0700

    melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -3,6 +3,16 @@ class Calculator
 static void Main (string[] args)
 	{
 	
+		int a = 10;
+		int b = 6;
+
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian: {0} x {1} = {2}", a, b, Perkalian(a, b));
+		Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	
 	}
 	
